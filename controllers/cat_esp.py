22d15e85fbb1f54f884fabdbb8ef1f81171fc27f
Mariano <mariano_ef@live.com>
2024-06-21 17:22:10 -0300

    der modificado

--- controllers/cat_esp.py
+++ controllers/cat_esp.py
@@ -6,10 +6,10 @@ categorias_bp = Blueprint('categorias', __name__)
 
 @categorias_bp.route('/categoria', methods=['GET'])
 def categoria_by_id():
-    return get_categoria_by_id(request.json["id"])
+    return get_categoria_by_id(request.json["id_espe"])
 
 
-@categorias_bp.route('/categorias', methods=['POST'])
+@categorias_bp.route('/categoria', methods=['POST'])
 def categorias_add():
     return add_categoria(request.json)
 
@@ -17,3 +17,8 @@ def categorias_add():
 @categorias_bp.route('/categorias', methods=['GET'])
 def categorias_get():
     return get_categorias()
+
+
+@categorias_bp.route('/categoria', methods=['PUT'])
+def categorias_modify():
+    return modify_categoria(request.json)
--- services/cat_esp.py
+++ services/cat_esp.py
@@ -5,15 +5,15 @@ from functions.id_generator import id_generator
 
 def get_categorias():
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM categorias_especialidades")
+    cursor.execute("SELECT * FROM especialidades")
     cat_esp = cursor.fetchall()
     cursor.close()
     return jsonify(cat_esp)
 
 
-def get_categoria_by_id(id: str):
+def get_categoria_by_id(id_espe: str):
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM categorias_especialidades WHERE id_esca = %s", (id,))
+    cursor.execute("SELECT * FROM especialidades WHERE id_espe = %s", (id_espe,))
     cat_esp = cursor.fetchone()
     cursor.close()
     return jsonify(cat_esp)
@@ -24,7 +24,17 @@ def add_categoria(data):
     new_id = id_generator("cat")
     while get_categoria_by_id(new_id) is None:
         new_id = id_generator("cat")
-    cursor.execute("INSERT INTO categorias_especialidades (id_esca, categoria) VALUES (%s, %s)", (new_id, data['categoria'],))
+    cursor.execute("INSERT INTO especialidades (id_espe, descripcion) VALUES (%s, %s)", (new_id, data['descripcion'],))
     mysql.connection.commit()
     cursor.close()
     return jsonify({"message": "Datos cargados"}), 400
+
+
+def modify_categoria(data):
+    cursor = mysql.connection.cursor()
+    cursor.execute("UPDATE especialidades SET descripcion = %s WHERE id_espe = %s", (data['descripcion'], data['id_espe'],))
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({"message": "Datos modificados"}), 400
+
+
